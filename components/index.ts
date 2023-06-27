b2bc44cc60d6d4b9a65abbabf523da4ecc82554b
CiaoRaviRaj <ravir@zignuts.com>
2023-06-27 13:43:08 +0530

    finall commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufeacturer from "./SearchManufeacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CardDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  SearchManufeacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
